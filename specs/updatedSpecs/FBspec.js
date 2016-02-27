ff631e2d954fe7ff8b702096d3fe2c3bb9ac8989
Nadine Ott <nadineott7@yahoo.com>
2016-02-27 11:15:35 -0800

    [FBspec.js] modified specs [specRunner.html]added files to test on

--- specs/updatedSpecs/FBspec.js
+++ specs/updatedSpecs/FBspec.js
@@ -11,8 +11,16 @@ describe("format date function", function() {
   it ("should be a function", function() {
     expect(typeof formatDate).toEqual('function');
   })
-  it ("should returned formatted date", function() {
-    expect(formatDate()).toContain("/");
-  })
+  //   it ("addMarker should be a function", function() {
+  //   expect(typeof addMarker).toEqual('function');
+  // })
+  // it ("should returned formatted date", function() {
+  //   expect(formatDate()).toContain("/");
+  // })
 });
 
+// describe("Google Maps Initializer", function() {
+//   it ("should have an asyncLoad function", function() {
+//     expect(typeof asyncLoad).toEqual('functiona');
+//   });
+// });
